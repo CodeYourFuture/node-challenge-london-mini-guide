5e8f4176d6468199892b958c918f31e0befc2ece
Anthony Mogotlane <almogotlane@gmail.com>
2022-11-20 12:54:45 +0200

    modified the get method with root directory

--- server/server.js
+++ server/server.js
@@ -7,17 +7,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send([
         {
-            "/pharmacies": "returns an array of pharmacies in a specific area"
-        },
-        {
-            "/colleges": "colleges list for stratford"
-        },
-        {
-            "/doctors": "returns doctors list for stratford"
-        },
-        {
-            "/hospitals": "returns hospitals list for stratford"
-        }	
+            "/pharmacies": "returns an array of pharmacies in a specific area",
+            "/colleges": "returns an array of colleges in a specific area",
+            "/doctors": "returns an array of doctors in a specific area",
+            "/hospitals": "returns an array of hospitals in a specific area"
+        }
     ])
 })
 
