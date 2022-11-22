732a12e518cedaa6249334cb80a74787a9ea2c1c
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2022-11-22 19:09:00 +0200

    ZA2-Christian-Mfuke-node-challenge-london-mini-guide
    Level 200 completed

--- server.js
+++ server.js
@@ -7,15 +7,30 @@ app.use(express.json());
 const Harrow = require("./data/Harrow.json");
 const Heathrow = require("./data/Heathrow.json");
 const Stratford = require("./data/Stratford.json");
+console.log(Stratford.pharmacies);
 
-//lEVELL 100
+//lEVEL 100
 app.get("/", (req, res) => {
   res.send({
-    "/pharmcies": "retruns an array of pharmacies in a specific area",
+    "/pharmacies": "retruns an array of pharmacies in a specific area",
     "/Doctors": "retruns an array of Doctor in a specific area",
     "/Colleges": "retruns an array of Colleges in a specific area",
     "/Hospitals": "retruns an array of Hospitals in a specific area",
   });
+});
+
+//LEVEL200
+app.get("/pharmacies", (req, res) => {
+  res.send(Stratford.pharmacies);
+});
+app.get("/doctors", (req, res) => {
+  res.send(Stratford.doctors);
+});
+app.get("/colleges", (req, res) => {
+  res.send(Stratford.colleges);
+});
+app.get("/hospitals", (req, res) => {
+  res.send(Stratford.hospitals);
 });
 
 app.listen(port, () => {
