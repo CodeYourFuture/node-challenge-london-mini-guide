da59f3a573c0c3be2e6f209a13279863916048d1
Tresor Bipa <tresorbipa3@gmail.com>
2022-11-28 14:20:55 +0200

    Level 100 completed

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json()); // Middleware to handle JSON files
+
+
+// require the JSON files in the data folder
+const Harrow = require('./data/Harrow.json');
+const Heathrow = require('./data/Heathrow.json');
+const Stratford = require('./data/Stratford.json');
+
+// Level 100:  / respond with the routes you are planing to implement
+app.get('/', (req, res) => {
+    console.log("Returning all the routes");
+    res.send(
+        {
+            SupportedRoutes : [
+                '/Pharmacies',
+                '/Doctors',
+                '/Colleges',
+                '/Hospitals',
+            ],
+            Version:"0.0.0.1",
+            Author: "Tresor && Riyaaz"
+        });
+})
+
+// // Level 200:  working for only one city Stratford
+
+// app.get('/pharmacies', () => {
+//     console.log('Returning all the Pharmacies in Stratford City');
+//     res.send(pharmacies.Stratford);
+// })
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
