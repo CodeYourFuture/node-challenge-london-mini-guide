3232c03cda967db67dd2dcb7d8a89a3a2f5b5dc6
Tresor Bipa <tresorbipa3@gmail.com>
2022-11-28 20:11:44 +0200

    level 300 completed

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json()); // Middleware to handle JSON files
+app.use(express.urlencoded({ extended: false }));
 
 
 // require the JSON files in the data folder
-const Harrow = require('./data/Harrow.json');
-const Heathrow = require('./data/Heathrow.json');
-const Stratford = require('./data/Stratford.json');
+ let Harrow = require('./data/Harrow.json');
+ let Heathrow = require('./data/Heathrow.json');
+ let Stratford = require('./data/Stratford.json');
 
 // Level 100:  / respond with the routes you are planing to implement
 app.get('/', (req, res) => {
@@ -26,26 +27,108 @@ app.get('/', (req, res) => {
 })
 
 // Level 200:  working for only one city Stratford
-
 app.get('/pharmacies', (req, res) => {
     console.log('Returning all the Pharmacies in Stratford City');
-    res.send(Stratford.pharmacies);
+    res.json(Stratford.pharmacies);
 })
 
 app.get('/colleges', (req, res) => {
     console.log('Returning all the Colleges in Stratford City');
-    res.send(Stratford.colleges);
+    res.json(Stratford.colleges);
 })
 
 app.get('/doctors', (req, res) => {
     console.log('Returning all the Doctors in Stratford City');
-    res.send(Stratford.doctors);
+    res.json(Stratford.doctors);
 })
 
 app.get('/hospitals', (req, res) => {
     console.log('Returning all the Hospitals in Stratford City');
-    res.send(Stratford.hospitals);
+    res.json(Stratford.hospitals);
 })
+
+// Level 300: You should be able to return data based on any city that is passed to the server
+app.get('/:city/pharmacies', (req, res) => {
+    let cityData = req.params.city;
+
+    if(cityData === "Stratford") {
+     res.json(Stratford.pharmacies);
+
+    } else if(cityData === "Heathrow") {
+        res.json(Heathrow.pharmacies);
+
+    }else if(cityData === "Harrow") {
+        res.json(Harrow.pharmacies);
+
+    } else{
+        res.status(404).send('No result Find for this City Sorry!!!');
+    }
+
+})
+
+app.get('/:city/colleges', (req, res) => {
+    let cityData = req.params.city;
+
+    if(cityData === "Stratford") {
+     res.json(Stratford.colleges);
+
+    } else if(cityData === "Heathrow") {
+        res.json(Heathrow.colleges);
+
+    }else if(cityData === "Harrow") {
+        res.json(Harrow.colleges);
+
+    } else{
+        res.status(404).send('No result Find for this Colleges Sorry!!!');
+    }
+
+})
+
+app.get('/:city/doctors', (req, res) => {
+    let cityData = req.params.city;
+
+    if(cityData === "Stratford") {
+     res.json(Stratford.doctors);
+
+    } else if(cityData === "Heathrow") {
+        res.json(Heathrow.doctors);
+
+    }else if(cityData === "Harrow") {
+        res.json(Harrow.doctors);
+
+    } else{
+        res.status(404).send('No result Find for this Doctors Sorry!!!');
+    }
+
+})
+
+app.get('/:city/hospitals', (req, res) => {
+    let cityData = req.params.city;
+
+    if(cityData === "Stratford") {
+     res.json(Stratford.hospitals);
+
+    } else if(cityData === "Heathrow") {
+        res.json(Heathrow.hospitals);
+
+    }else if(cityData === "Harrow") {
+        res.json(Harrow.hospitals);
+
+    } else{
+        res.status(404).send('No result Find for this Hospitals Sorry!!!');
+    }
+
+})
+
+// Level 500: Make all of that in one single route as:
+app.get('/:city/:category', (req, res) => {
+   
+})
+
+
+
+
+
 
 
 
